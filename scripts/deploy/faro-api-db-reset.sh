6a130edb52a8856173535f35ee43905417ac26ad
Justin Hammond <justin.hammond@rackspace.com>
2013-07-30 17:33:36 +0000

    Activating the venv

--- scripts/deploy/faro-api-db-reset.sh
+++ scripts/deploy/faro-api-db-reset.sh
@@ -8,7 +8,10 @@ sleep 30
 echo "Dropping current DB and creating new one"
 mysql -u root --password=password -e "drop database $DB; create database $DB;"
 echo "Syncing database"
+cd /opt/faro/faro-api
+source .venv/bin/activate
 STATUS=`/opt/faro/faro-api/scripts/service-database-sync.sh`
 echo "Sync returned: $STATUS"
 echo "Restarting service"
+deactivate
 supervisorctl start faro_api
