b5c386e3bdef0bf7f93ece6023fea546ee072c05
Justin Hammond <justin.hammond@rackspace.com>
2013-07-30 17:42:25 +0000

    Added more responsive children kill wait

--- scripts/deploy/faro-api-db-reset.sh
+++ scripts/deploy/faro-api-db-reset.sh
@@ -4,7 +4,18 @@ echo "Resetting the database: $DB"
 echo "Stopping service"
 supervisorctl stop faro_api
 echo "Waiting for children to die"
-sleep 30
+PCOUNT=`ps aux | grep faro_api | grep gunicorn | wc -l`
+TIMEOUT=0
+MAX_TIMEOUT=35
+while $PCOUNT -ne 0; do
+    sleep 1
+    PCOUNT=`ps aux | grep faro_api | grep gunicorn | wc -l`
+    TIMEOUT=`expr $TIMEOUT + 1`
+    if [ $TIMEOUT -gt $MAX_TIMEOUT ]; then
+        echo "Timeout reached waiting for children to die."
+        exit 1
+    fi
+done
 echo "Dropping current DB and creating new one"
 mysql -u root --password=password -e "drop database $DB; create database $DB;"
 echo "Syncing database"
