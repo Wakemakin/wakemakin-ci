4523cc160c3d6f46a62c4165060dbd7e29edb021
Justin Hammond <justin.hammond@rackspace.com>
2013-07-30 18:56:21 +0000

    removed debug shit breaking all the things

--- scripts/deploy/release-checker.sh
+++ scripts/deploy/release-checker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Checking for release $1 at $2"
 PCOUNT=`ps aux | grep faro_api | grep gunicorn | wc -l`
 RELEASE=`curl -s $2`
 TIMEOUT=0
@@ -9,7 +8,6 @@ while [ $RELEASE -ne $1 ]; do
     RELEASE=`curl -s $2`
     TIMEOUT=`expr $TIMEOUT + 1`
     if [ $TIMEOUT -gt $MAX_TIMEOUT ]; then
-        echo "Timeout reached waiting for release to update."
         exit 1
     fi
 done
